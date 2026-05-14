84d84a0a095415a134101c861fbfcc54cd578caf
mo-Noel <noel.limpag@movember.com>
2026-05-14 14:12:51 +0800

    chore: untrack .next build cache

--- .next/dev/server/pages/api/scrape.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/pages/api/scrape.js")
-R.c("server/chunks/[root-of-the-server]__0b-s32s._.js")
-R.c("server/chunks/[root-of-the-server]__0a~1voh._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/scrape/index.ts [api] (ecmascript)\" } [api] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/src/pages/api/scrape/index.ts [api] (ecmascript)\" } [api] (ecmascript)").exports
--- .next/dev/server/pages/loadsite.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/loadsite.js")
-R.c("server/chunks/ssr/node_modules_next_dist_0ndqdvy._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__0sdopu.._.js")
-R.c("server/chunks/ssr/node_modules_129w7gn._.js")
-R.c("server/chunks/ssr/[externals]__0.o~_~l._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__10fyzbd._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/loadsite.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/src/pages/loadsite.tsx [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/src/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
